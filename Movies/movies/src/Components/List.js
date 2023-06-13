b25fa55661c2022e37245339bfd084f69d9cec6f
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2023-06-13 16:59:14 +0530

    :fire: axios, lifecycle methods(Mounting , Updating , Unmounting) , getting data from api

--- Movies/movies/src/Components/List.js
+++ Movies/movies/src/Components/List.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
-import { movies } from './getMovies'
+//import { movies } from './getMovies'
+import axios from 'axios'
 export default class List extends Component {
   constructor(){
     super();
     this.state = {
-      hover : ""
+      hover : "" , 
+      parr : [1], // ab tak mein konsee page pe hu or what page result am i showing
+      currPage : 3, 
+      movies : []
     }
   }
 
   handleEnter = (id) =>{
     this.setState({
-      hover : id
+      hover : id ,
+     
     })
   }
 
@@ -19,11 +24,23 @@ export default class List extends Component {
       hover : ""
     })
   }
+
+
+  async componentDidMount(){
+    console.log(this.state.currPage)
+   let ans =  await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=f5d6e9162514147762f2410576d4357a&language-en-US&page=${this.state.currPage}`);
+   console.log(ans.data);
+   this.setState({
+    movies : [...ans.data.results]
+   })
+  }
+
+
   render() { // jab bhi state set hogi render method call hoga
-     let movie = movies.results;     
+     //let movie = movies.results;     
     return (
       <>
-          {movie.length==0 ? 
+          {this.state.movies.length==0 ? 
                     <div className="spinner-border text-danger" role="status">
                               <span className="sr-only"></span>
                     </div> :
@@ -35,7 +52,7 @@ export default class List extends Component {
                                 {
                                   <div className="movies-list">
                                       {
-                                          movie.map((movieObj) => (
+                                          this.state.movies.map((movieObj) => (
                                               <div className="card movie-card" onMouseEnter={()=>this.handleEnter(movieObj.id)} onMouseLeave={this.handleLeave} key={movieObj.id}>
                                                 <img
                                                   src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`}
@@ -46,7 +63,7 @@ export default class List extends Component {
                                                 {/* <div className="card-body"> */}
                                                   <h4 className="card-title movie-title">{movieObj.original_title}</h4>
                                                   {/* <p className="card-text banner-text">{movieObj.overview}</p> */}
-                                                  <div class="button-wrapper movie-button" style =  {{display :  "flex" , width:"100%" , justifyContent : "center" }}>
+                                                  <div className="button-wrapper movie-button" style =  {{display :  "flex" , width:"100%" , justifyContent : "center" }}>
                                                         {
                                                           this.state.hover == movieObj.id && 
                                                           <a href="#" className="btn btn-primary">Add To Favourites</a> 
@@ -59,14 +76,17 @@ export default class List extends Component {
                                   </div>            
                                                                                                                   
                                 }
-                                    <div className = "pagination">
+
+                                <div className = "pagination">
                                       <nav aria-label="Page navigation example">
-                                          <ul class="pagination">
-                                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                          <ul className="pagination">
+                                            <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                                            {
+                                              this.state.parr.map((pageNum)=>(
+                                                <li className="page-item"><a className="page-link" href="#">{pageNum}</a></li>
+                                              ))
+                                            }
+                                            <li className="page-item"><a className="page-link" href="#">Next</a></li>
                                           </ul>
                                       </nav>
                                     </div>
