04330adf3d8fe71870eaa3f4eef31aa96131fdba
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2023-06-20 17:44:11 +0530

    :fire: favourites with style

--- Movies/movies/src/Components/Favourites.js
+++ Movies/movies/src/Components/Favourites.js
@@ -19,7 +19,7 @@ export default class Favourites extends Component {
     let genreId = { 28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary',  18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Sci-fi', 10770: 'TV', 53: 'Thriller', 10752: 'War', 37: 'Western' };
     return (
      <div class = "row">
-          <div class = "col-3" >
+          <div class = "col-3 favourites-list" >
                     <ul class="list-group">
                               <li class="list-group-item active">All Genre</li>
                               <li class="list-group-item">Fantasy</li>
@@ -29,7 +29,7 @@ export default class Favourites extends Component {
 
           </div>
 
-          <div class = "col"  >
+          <div class = "col favourites-table"  >
                     <div class = "row">
                               <input type = "text" className = "col" placeholder = "Search"></input>
                               <input type = "number" className = "col" placeholder = "5"></input>
@@ -55,6 +55,7 @@ export default class Favourites extends Component {
                                                                         <td>{genreId[movieObj.genre_ids[0]]}</td>
                                                                         <td>{movieObj.popularity}</td>
                                                                         <td>{movieObj.vote_average}</td>
+                                                                        <td><button class = "btn btn-outline-danger">Delete</button></td>
                                                                     </tr>
                                                                 );
                                                             })
