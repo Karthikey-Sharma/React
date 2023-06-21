e8c6cbdba1da43af800568ae5ff7bcdf647ba472
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2023-06-21 13:40:20 +0530

    :construction: react-router-dom

--- Movies/movies/src/Components/Navbar.js
+++ Movies/movies/src/Components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 export default class Navbar extends Component {
   render() {
     return (
       <div style = {{display : "flex" , background : "white" , color : "#2268FF", padding : "1rem" , justifyContent : "center" ,alignItems : "center"}}>
-          <h1>Movies App</h1>
-          <h1 style = {{marginLeft : "2rem"}}>Navbar</h1>
+
+          <Link to ="/" style = {{textDecoration : "none"}}> <h1>Movies App</h1></Link>
+          <Link to = "/fav" style = {{textDecoration : "none"}}><h1 style = {{marginLeft : "2rem"}}>Favourites</h1></Link>
+          
       </div>
     )
   }
